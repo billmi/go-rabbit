0cc04ba2e914dece6f2c82f572ec2b5bc3884304
yangxiancheng@commasmart.com <yangxiancheng@commasmart.com>
2020-03-27 18:16:08 +0800

    update

--- main.go
+++ main.go
@@ -7,33 +7,35 @@ import (
 /*
 	支持断线重连
 	失败重发
+	使用时最好读下代码,基本需要支持的已全支持,也可以自己添加
     @author : Bill
 */
 func main() {
-	var(
-		addr = "amqp://guest:guest@localhost:5672/"
+	var (
+		addr  = "amqp://guest:guest@localhost:5672/"
 		queue = "testQueue"
-		exchange = "test_exchange"
+		exchange  = "test_exchange"
 		routerKey = "/test"
-		msg = "test1!"
+		msg = "123123!"
 
 		//delay
-		delayQueue = "delay_queue"
-		delayExchange = "delay_exchange"
+		delayQueue     = "delay_queue"
+		delayExchange  = "delay_exchange"
 		delayRouterKey = "delay_exchange"
-		prefix = "v1_prefix"
-		sep = "_"
-		eType = "F"
+		prefix         = "v1_prefix"
+		sep            = "_"
+		eType          = "F"
 		_ttl = 60 * 1000
 	)
 
-	var rabbitProduct1 = rabbit.NewRabbitProduct(addr,_ttl,prefix,sep,delayExchange,delayQueue,delayRouterKey)
+	var rabbitProduct1 = rabbit.NewRabbitProduct(addr, _ttl, prefix, sep, delayExchange, delayQueue, delayRouterKey)
 	//注册回收
 	go rabbitProduct1.InitDefdelay(false)
 	go rabbitProduct1.InitDefdelay(true)
-	go rabbitProduct1.RegisterDelayWithPreFix("delay_queue","delay_exchange","delay_exchange")
+	go rabbitProduct1.RegisterDelayWithPreFix("delay_queue", "delay_exchange", "delay_exchange")
 
 	// ttl 为  设置死信回收时间  >0 则回收
-	rabbitProduct1.PubMessage(true,eType,queue,exchange,routerKey,msg,rabbitProduct1.GetBool(1),rabbitProduct1.GetBool(0),_ttl)
+	rabbitProduct1.PubMessage(true, eType, queue, exchange, routerKey, msg, rabbitProduct1.GetBool(1), rabbitProduct1.GetBool(0), _ttl)
 
+	rabbitProduct1.QueueSimple(queue, msg, true, false)
 }
--- rabbit/rabbit_product.go
+++ rabbit/rabbit_product.go
@@ -1,60 +1,58 @@
 package rabbit
 
 import (
-	"log"
-	"github.com/streadway/amqp"
-	"time"
-	"os"
 	"errors"
+	"github.com/streadway/amqp"
+	"log"
+	"os"
+	"time"
 )
-
 
 /**
-	构件支持断线重连,与失败重发机制
-	Rabbit
-	author : Bill
- */
+构件支持断线重连,与失败重发机制
+Rabbit
+author : Bill
+*/
 
 const (
-	reconnectDelay = 2 * time.Second  // 连接断开后多久重连
-	resendDelay = 5 * time.Second     // 消息发送失败后，多久重发
-	resendTime = 3    // 消息重发次数
-	maxWaitNum = 5    //最大等待次数
-	defSep = "_"
+	reconnectDelay = 2 * time.Second // 连接断开后多久重连
+	resendDelay    = 5 * time.Second // 消息发送失败后，多久重发
+	resendTime     = 3               // 消息重发次数
+	maxWaitNum     = 5               //最大等待次数
+	defSep         = "_"
 )
 
-
 var (
-	Rabbit *RabbitProduct
+	Rabbit           *RabbitProduct
 	errNotConnected  = errors.New("Not connected to the producer")
 	errAlreadyClosed = errors.New("Already closed: not connected to the producer")
 )
 
 type RabbitProduct struct {
-	Addr            string
-	Connection    	*amqp.Connection
-	Channel       	*amqp.Channel
-	Ttl           	int
-	isConnected   	bool
-	done          	chan bool
-	notifyClose   	chan *amqp.Error
+	Addr        string
+	Connection  *amqp.Connection
+	Channel     *amqp.Channel
+	Ttl         int
+	isConnected bool
+	done        chan bool
+	notifyClose chan *amqp.Error
 	//notifyConfirm 	chan amqp.Confirmation
-	logger          *log.Logger
-	DelayExchange   string
-	DelayQueue      string
-	DelayRoute      string
-	PreFix          string
-	Sep             string
+	logger        *log.Logger
+	DelayExchange string
+	DelayQueue    string
+	DelayRoute    string
+	PreFix        string
+	Sep           string
 }
 
 //实体
-func NewRabbitProduct(addr string,ttl int,prefix string,sep string,delayExchange string,delayQueue string,delayRoute string) *RabbitProduct {
+func NewRabbitProduct(addr string, ttl int, prefix string, sep string, delayExchange string, delayQueue string, delayRoute string) *RabbitProduct {
 	rabbitProduct := RabbitProduct{
 		logger: log.New(os.Stdout, "", log.LstdFlags),
 		done:   make(chan bool),
 	}
 	rabbitProduct.Sep = sep
-	if sep == ""{
+	if sep == "" {
 		rabbitProduct.Sep = defSep
 	}
 	rabbitProduct.PreFix = prefix
@@ -62,16 +60,15 @@ func NewRabbitProduct(addr string,ttl int,prefix string,sep string,delayExchange
 	rabbitProduct.DelayQueue = delayQueue
 	rabbitProduct.DelayRoute = delayRoute
 	rabbitProduct.Addr = addr
-	go rabbitProduct.handleReconnect(addr,ttl)
+	go rabbitProduct.handleReconnect(addr, ttl)
 	return &rabbitProduct
 }
 
-
-func (p *RabbitProduct) handleReconnect(addr string,ttl int) {
+func (p *RabbitProduct) handleReconnect(addr string, ttl int) {
 	for {
-		if  p.isConnected == false || p.Connection.IsClosed() {
+		if p.isConnected == false || p.Connection.IsClosed() {
 			log.Println("Amqp to connect")
-			if ok,_  := p.Conn(addr,ttl); !ok  {
+			if ok, _ := p.Conn(addr, ttl); !ok {
 				log.Println("Failed to connect. Retrying...")
 			}
 		}
@@ -88,22 +85,21 @@ func (p *RabbitProduct) handleReconnect(addr string,ttl int) {
 	}
 }
 
-
-func (p *RabbitProduct)Conn(addr string,ttl int) (bool,error){
-	conn,err := amqp.Dial(addr)
-	if err != nil{
-		return false,err
+func (p *RabbitProduct) Conn(addr string, ttl int) (bool, error) {
+	conn, err := amqp.Dial(addr)
+	if err != nil {
+		return false, err
 	}
 	p.Connection = conn
-	channel,err := conn.Channel()
-	if err != nil{
-		return false,err
+	channel, err := conn.Channel()
+	if err != nil {
+		return false, err
 	}
 	p.Ttl = ttl
-	p.changeConnection(conn,channel)
+	p.changeConnection(conn, channel)
 	p.isConnected = true
 	log.Println("Connected!")
-	return true,nil
+	return true, nil
 }
 
 func (p *RabbitProduct) changeConnection(connection *amqp.Connection, channel *amqp.Channel) {
@@ -114,193 +110,192 @@ func (p *RabbitProduct) changeConnection(connection *amqp.Connection, channel *a
 	p.Channel.NotifyClose(p.notifyClose)
 }
 
-func (p *RabbitProduct)SetTtl(ttl int){
+func (p *RabbitProduct) SetTtl(ttl int) {
 	p.Ttl = ttl
 }
 
-func (p *RabbitProduct)GetTtl()int{
+func (p *RabbitProduct) GetTtl() int {
 	return p.Ttl
 }
 
-func (p *RabbitProduct)GetBool(sign int)bool{
-	if sign == 0{
+func (p *RabbitProduct) GetBool(sign int) bool {
+	if sign == 0 {
 		return false
 	}
 	return true
 }
 
-func (p *RabbitProduct)GetType(tTag string)string{
+func (p *RabbitProduct) GetType(tTag string) string {
 	var _t = ""
 	switch tTag {
 	case "H":
-		_t = amqp.ExchangeHeaders
+		_t = amqp.ExchangeHeaders  //  routingkey will be not uesd   header模式与routing不同的地方在于，header模式取消routingkey，使用header中的 key/value（键值对）匹配队列。
 		break
 	case "F":
-		_t = amqp.ExchangeFanout
+		_t = amqp.ExchangeFanout   // p / s
 		break
 	case "T":
-		_t = amqp.ExchangeTopic
+		_t = amqp.ExchangeTopic    //   example  =>  *.critical   [wide than fanout]
 		break
 	case "D":
-		_t = amqp.ExchangeDirect
+		_t = amqp.ExchangeDirect   //   direct
 		break
 	default:
-		_t = amqp.ExchangeFanout
+		_t = amqp.ExchangeFanout	// p / s
 	}
 	return _t
 }
 
-
 //推送消息
-func (p *RabbitProduct)PubMessage(usePreFix bool,eType string,queue string,exchange string,route string,msg string,durable bool,autoDelete bool,ttl int)(bool,error){
-	if ok,err := p.waitConn();!ok{
-		return ok,err
+func (p *RabbitProduct) PubMessage(usePreFix bool, eType string, queue string, exchange string, route string, msg string, durable bool, autoDelete bool, ttl int) (bool, error) {
+	if ok, err := p.waitConn(); !ok {
+		return ok, err
 	}
 	var prefix = ""
-	if usePreFix{
+	if usePreFix {
 		prefix = p.PreFix + p.Sep
 	}
-	var(
+	var (
 		channel = p.Channel
 		defType = p.GetType(eType)
-		err error
+		err     error
 
 		_Exchage = prefix + exchange
-		_Queue = prefix + queue
-		_Route = prefix + route
+		_Queue   = prefix + queue
+		_Route   = prefix + route
 	)
-	if queue == ""{
+	if queue == "" {
 		_Queue = ""
 	}
-	if route == ""{
+	if route == "" {
 		_Route = ""
 	}
-	err = channel.ExchangeDeclare(_Exchage,defType, durable, autoDelete, false, false, nil)
-	if err != nil{
-		return false,err
+	err = channel.ExchangeDeclare(_Exchage, defType, durable, autoDelete, false, false, nil)
+	if err != nil {
+		return false, err
 	}
 
-	err = p.QueueDeclareDelay(_Queue,durable,autoDelete,ttl)
-	if err != nil{
-		return false,err
+	err = p.QueueDeclareDelay(_Queue, durable, autoDelete, ttl)
+	if err != nil {
+		return false, err
 	}
 
 	err = channel.QueueBind(_Queue, _Route, _Exchage, false, nil)
 
-	if err != nil{
-		return false,err
+	if err != nil {
+		return false, err
 	}
 
-	err = p.UnsafePush(_Exchage,_Route,msg)
-	if err != nil{
+	err = p.UnsafePush(_Exchage, _Route, msg)
+	if err != nil {
 		var currentTime = 0
 		for {
-			err := p.UnsafePush(_Exchage,_Route,msg)
+			err := p.UnsafePush(_Exchage, _Route, msg)
 			if err != nil {
 				p.logger.Println("Push failed. Retrying...")
 				currentTime += 1
 				if currentTime < resendTime {
 					continue
-				}else {
-					return false,err
+				} else {
+					return false, err
 					break //safe
 				}
 			}
 			time.Sleep(resendDelay)
 		}
-		return false,err
+		return false, err
 	}
-	return true,nil
+	return true, nil
 }
 
 //设置队列回收
-func (p *RabbitProduct)QueueDeclareDelay(queue string,durable bool,auto_delete bool,ttl int) error{
-	if ok,err := p.waitConn();!ok{
+func (p *RabbitProduct) QueueDeclareDelay(queue string, durable bool, auto_delete bool, ttl int) error {
+	if ok, err := p.waitConn(); !ok {
 		return err
 	}
 	var (
-		_ttl = p.Ttl
+		_ttl    = p.Ttl
 		channel = p.Channel
-		err error
+		err     error
 	)
-	if ttl > 0{
-		if _ttl > 0{
+	if ttl > 0 {
+		if _ttl > 0 {
 			_ttl = ttl
 		}
 		var table = amqp.Table{
-			"x-dead-letter-exchange" : p.PreFix + p.DelayExchange,
-			"x-dead-letter-routing-key" : p.PreFix + p.DelayRoute,
-			"x-message-ttl" : _ttl,
+			"x-dead-letter-exchange":    p.PreFix + p.DelayExchange,
+			"x-dead-letter-routing-key": p.PreFix + p.DelayRoute,
+			"x-message-ttl":             _ttl,
 		}
 		_, err = channel.QueueDeclare(queue, durable, auto_delete, false, false, table)
-	}else{
+	} else {
 		_, err = channel.QueueDeclare(queue, durable, auto_delete, false, false, nil)
 	}
 	return err
 }
 
 //删除队列
-func (p *RabbitProduct)DeleteQueue(usePrefix bool,queueName string,use bool,empty bool)(int,error){
-	if ok,err := p.waitConn();!ok{
-		return 0,err
+func (p *RabbitProduct) DeleteQueue(usePrefix bool, queueName string, use bool, empty bool) (int, error) {
+	if ok, err := p.waitConn(); !ok {
+		return 0, err
 	}
 	_queue_name := queueName
-	if usePrefix{
+	if usePrefix {
 		_queue_name = p.PreFix + p.Sep + _queue_name
 	}
-	return p.Channel.QueueDelete(_queue_name,use,empty,false)
+	return p.Channel.QueueDelete(_queue_name, use, empty, false)
 }
 
 //删除交换机(暂时不提供)
-func (p *RabbitProduct)DeleteExchange(usePrefix bool,exchangeName string,use bool,empty bool)(error){
-	if ok,err := p.waitConn();!ok{
+func (p *RabbitProduct) DeleteExchange(usePrefix bool, exchangeName string, use bool, empty bool) error {
+	if ok, err := p.waitConn(); !ok {
 		return err
 	}
 	_exchange_name := exchangeName
-	if usePrefix{
+	if usePrefix {
 		_exchange_name = p.PreFix + p.Sep + _exchange_name
 	}
-	return p.Channel.ExchangeDelete(_exchange_name,use,false)
+	return p.Channel.ExchangeDelete(_exchange_name, use, false)
 }
 
 //解绑
-func (p *RabbitProduct)QueueUnbind(usePrefix bool,name string,route string,exchange string)error{
-	if ok,err := p.waitConn();!ok{
+func (p *RabbitProduct) QueueUnbind(usePrefix bool, name string, route string, exchange string) error {
+	if ok, err := p.waitConn(); !ok {
 		return err
 	}
 	_name := name
 	_route := route
 	_exchange := exchange
-	if usePrefix{
-		_name     =  p.PreFix + p.Sep + _name
-		_route    =  p.PreFix + p.Sep + _route
-		_exchange =  p.PreFix + p.Sep + _exchange
+	if usePrefix {
+		_name = p.PreFix + p.Sep + _name
+		_route = p.PreFix + p.Sep + _route
+		_exchange = p.PreFix + p.Sep + _exchange
 	}
-	return p.Channel.QueueUnbind(_name,_route,_exchange,nil)
+	return p.Channel.QueueUnbind(_name, _route, _exchange, nil)
 }
 
 //Push
-func (p *RabbitProduct) UnsafePush(exchange string,routeKey string,data string) error {
-	if ok,err := p.waitConn();!ok{
+func (p *RabbitProduct) UnsafePush(exchange string, routeKey string, data string) error {
+	if ok, err := p.waitConn(); !ok {
 		return err
 	}
 	return p.Channel.Publish(exchange, routeKey, false, false,
 		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(data),
-			DeliveryMode:amqp.Persistent,
-			Timestamp:  time.Now(),
+			ContentType:  "text/plain",
+			Body:         []byte(data),
+			DeliveryMode: amqp.Persistent,
+			Timestamp:    time.Now(),
 		},
 	)
 }
 
 //等待重连
-func (p *RabbitProduct)waitConn()(bool,error){
+func (p *RabbitProduct) waitConn() (bool, error) {
 	var _flag = false
 	var i = 1
 Loop:
 	for {
-		if i == maxWaitNum{
+		if i == maxWaitNum {
 			break Loop
 		}
 		if _flag != p.isConnected {
@@ -311,70 +306,94 @@ Loop:
 		time.Sleep(1 * time.Second)
 	}
 	if !p.isConnected {
-		return _flag,errNotConnected
+		return _flag, errNotConnected
 	}
-	return _flag,nil
+	return _flag, nil
 }
 
 //回收队列注册
-func (p *RabbitProduct)RegisterDelay(delayQueue string,delayExchange string,routeKey string) (bool,error){
-	if ok,err := p.waitConn();!ok{
-		return ok,err
+func (p *RabbitProduct) RegisterDelay(delayQueue string, delayExchange string, routeKey string) (bool, error) {
+	if ok, err := p.waitConn(); !ok {
+		return ok, err
 	}
-	var(
-		err error
+	var (
+		err   error
 		Queue string
-		Ex string
+		Ex    string
 		Route string
 	)
-	if delayQueue != ""{
+	if delayQueue != "" {
 		Queue = delayQueue
 	}
-	if delayExchange != ""{
+	if delayExchange != "" {
 		Ex = delayExchange
 	}
-	if routeKey != ""{
-		Route =  routeKey
+	if routeKey != "" {
+		Route = routeKey
 	}
 	err = p.Channel.ExchangeDeclare(Ex, amqp.ExchangeDirect, true, true, false, false, nil)
-	if err != nil{
-		return false,err
+	if err != nil {
+		return false, err
 	}
-	_,err = p.Channel.QueueDeclare(Queue , true, false, false, false, nil)
-	if err != nil{
-		return false,err
+	_, err = p.Channel.QueueDeclare(Queue, true, false, false, false, nil)
+	if err != nil {
+		return false, err
 	}
-	err = p.Channel.QueueBind(Queue,Route,Ex,false,nil)
-	if err != nil{
-		return false,err
+	err = p.Channel.QueueBind(Queue, Route, Ex, false, nil)
+	if err != nil {
+		return false, err
 	}
-	return true,nil
+	return true, nil
 }
 
 //默认回收队列注册
-func (p *RabbitProduct)InitDefdelay(usePreFix bool) (bool,error){
+func (p *RabbitProduct) InitDefdelay(usePreFix bool) (bool, error) {
 	var prefix = ""
-	if usePreFix{
+	if usePreFix {
 		prefix = p.PreFix + p.Sep
 	}
 	var (
-		DelayQueue = prefix + p.DelayQueue
+		DelayQueue    = prefix + p.DelayQueue
 		DelayExchange = prefix + p.DelayExchange
-		DelayRoute = prefix + p.DelayRoute
+		DelayRoute    = prefix + p.DelayRoute
 	)
-	return p.RegisterDelay(DelayQueue,DelayExchange,DelayRoute)
+	return p.RegisterDelay(DelayQueue, DelayExchange, DelayRoute)
 }
 
+/**
+	QueueSimple Queue push
+	TODO :: Test
+ */
+func (p *RabbitProduct) QueueSimple(queue string,  msg string, durable bool, autoDelete bool) (bool, error) {
+	if ok, err := p.waitConn(); !ok {
+		return ok, err
+	}
+	q, err := p.Channel.QueueDeclare(
+		queue,      // name
+		durable,    // durable
+		autoDelete, // delete when unused
+		false,      // exclusive
+		false,      // no-wait
+		nil,        // arguments   if you need
+	)
+	if err != nil {
+		return false, err
+	}
+	if err := p.UnsafePush("", q.Name, msg); err != nil {
+		return false, err
+	}
+	return true, nil
+}
 
 //外部调用
-func (p *RabbitProduct)RegisterDelayWithPreFix(delayQueue string,delayExchange string,routeKey string) (bool,error){
+func (p *RabbitProduct) RegisterDelayWithPreFix(delayQueue string, delayExchange string, routeKey string) (bool, error) {
 	var (
-		pre = p.PreFix + p.Sep
-		DelayQueue = pre + delayQueue
+		pre           = p.PreFix + p.Sep
+		DelayQueue    = pre + delayQueue
 		DelayExchange = pre + delayExchange
-		route = pre + routeKey
+		route         = pre + routeKey
 	)
-	return p.RegisterDelay(DelayQueue,DelayExchange,route)
+	return p.RegisterDelay(DelayQueue, DelayExchange, route)
 }
 
 // 关闭连接
